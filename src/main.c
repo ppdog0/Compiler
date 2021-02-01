5c4df153decae58af205c60ff202f70f07031c73
ppdog <ppdoooog@163.com>
2021-02-01 23:49:10 +0800

    bytecode

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
-#include"include/common.h"
+#include"common.h"
+#include"chunk.h"
+#include"debug.h"
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
